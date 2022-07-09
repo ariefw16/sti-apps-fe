cecdb5567dc27192f278303f05177f503b1c312d
ariefw16 <wahyuutomo.arief@gmail.com>
2022-07-09 10:03:15 +0700

    update

--- src/features/Zoom/index.tsx
+++ src/features/Zoom/index.tsx
@@ -1,8 +1,12 @@
 import { Box, LoadingOverlay } from "@mantine/core";
-import { useState } from "react";
+import { showNotification } from "@mantine/notifications";
+import { useEffect, useState } from "react";
+import { useRecoilState, useSetRecoilState } from "recoil";
 import PageTitleComponent from "../../components/common/PageTitle";
 import { PageTitleBreadcrumbs } from "../../types/pagetitle.type";
 import ZoomAccountListCard from "./components/list/ZoomAccountListCard";
+import { fetchZoomAccount } from "./utils/service";
+import { zoomListCountState, zoomListState } from "./utils/store";
 
 export default function ZoomAccountPage() {
   const breadcrumbs: PageTitleBreadcrumbs[] = [
@@ -15,6 +19,20 @@ export default function ZoomAccountPage() {
     }
   ]
   const [loading, setLoading] = useState(false)
+  const setAccount = useSetRecoilState(zoomListState)
+  const setRowCount = useSetRecoilState(zoomListCountState)
+
+  useEffect(() => {
+    setLoading(true)
+    fetchZoomAccount({}).then(res => {
+      setAccount(res.data)
+      setRowCount(res.rowCount)
+    }).catch(e => {
+      showNotification({ title: 'Fetch Zoom Account', message: `Error! ${e.message}`, color: 'red' })
+    }).finally(() => {
+      setLoading(false)
+    })
+  }, [])
 
   return <>
     <PageTitleComponent title="Zoom Account Management" breadcrumbs={breadcrumbs} />
